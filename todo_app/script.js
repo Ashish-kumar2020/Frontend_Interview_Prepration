8093537e1f9ddf31f43ea618fe4fad56b29bb6fb
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-03-01 13:27:54 +0530

    display todo dynamically

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,1 +1,24 @@
 console.log("Todo Web App");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const todoInput = document.querySelector("#todoinput");
+  const addTodoBtn = document.querySelector(".addTodoBtn");
+  const todoList = document.querySelector(".todoList");
+
+  addTodoBtn.addEventListener("click", function (e) {
+    const userInput = todoInput.value;
+    console.log(userInput);
+
+    const list = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
+
+    deleteBtn.innerHTML = "Delete";
+    editBtn.innerHTML = "Edit ";
+    list.innerHTML = userInput;
+    list.append(deleteBtn);
+    list.append(editBtn);
+    todoList.appendChild(list);
+    todoInput.value = "";
+  });
+});
