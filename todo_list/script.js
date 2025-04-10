5356db884843e07568f4cd42cfdb4c5cbfd16f7a
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-04-10 22:50:12 +0530

    Added edit todo functionality

--- todo_list/script.js
+++ todo_list/script.js
@@ -9,14 +9,55 @@ const todoLists = document.querySelector(".todoLists");
 addTodo.addEventListener("click", createTodo);
 
 function createTodo() {
+  if (inputTodo.value.trim() === "") {
+    alert("Task cannot be empty!");
+    return;
+  }
   const li = document.createElement("li");
   const deleteBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
   deleteBtn.innerText = "Delete";
+  editBtn.innerText = "Edit";
+  deleteBtn.id = inputTodo.value;
+  editBtn.id = inputTodo.value;
   li.innerHTML = inputTodo.value;
 
+  //   Delete Functionality
   deleteBtn.addEventListener("click", () => {
     li.remove();
   });
-  li.appendChild(deleteBtn);
+
+  //   edit Functionality
+
+  editBtn.addEventListener("click", () => {
+    editTodo(li);
+  });
+  inputTodo.value = "";
+  li.append(deleteBtn, editBtn);
+
   todoLists.appendChild(li);
+}
+
+function editTodo(li) {
+  const currentText = li.firstChild.textContent;
+  console.log(currentText);
+  const deleteBtn = li.querySelector("button:nth-of-type(1)");
+  const editBtn = li.querySelector("button:nth-of-type(2)");
+  const saveBtn = document.createElement("button");
+  saveBtn.textContent = "Save";
+  const inputEle = document.createElement("input");
+  inputEle.type = "text";
+  inputEle.value = currentText;
+
+  li.innerHTML = "";
+  li.appendChild(inputEle);
+  li.appendChild(saveBtn);
+  saveBtn.addEventListener("click", () => {
+    if (inputEle.value.trim() === "") {
+      alert("Task cannot be empty!");
+      return;
+    }
+    li.innerHTML = inputEle.value;
+    li.append(deleteBtn, editBtn);
+  });
 }
